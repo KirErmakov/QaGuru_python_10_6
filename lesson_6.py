f11165ee1a46a7cd9bafa4f9583a6db8acfef976
Kirill Ermakov <lvstrng161@gmail.com>
2024-02-15 15:56:56 +0400

    updated lesson_6.py

--- lesson_6.py
+++ lesson_6.py
@@ -74,24 +74,21 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
-def readable_fuction(func_name, **kwargs):
-    formatted_name = func_name.replace('_', ' ').title()
+def readable_function(func, **kwargs):
+    formatted_name = func.__name__.replace('_', ' ').title()
     args_names = ', '.join(map(str, kwargs.values()))
     return f'{formatted_name} [{args_names}]'
 
 def open_browser(browser_name):
-    f_name = open_browser.__name__
-    actual_result = readable_fuction(f_name, browser_name=browser_name)
+    actual_result = readable_function(open_browser, browser_name=browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    f_name = go_to_companyname_homepage.__name__
-    actual_result = readable_fuction(f_name, page_url=page_url)
+    actual_result = readable_function(go_to_companyname_homepage, page_url=page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    f_name = find_registration_button_on_login_page.__name__
-    actual_result = readable_fuction(f_name, page_url=page_url, button_text=button_text)
+    actual_result = readable_function(find_registration_button_on_login_page, page_url=page_url, button_text=button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
